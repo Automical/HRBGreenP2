65d7f41f1eb228eb52cac6c0ea3195c10f981faa
jzarger <jzarger@umich.edu>
2017-04-13 19:26:50 -0400

    this 3d plot is concerning

--- inverse.py
+++ inverse.py
@@ -24,12 +24,12 @@ class Arm( object ):
     self.l8 = 5
     
   def angFromEnd(self,x,y):
-    ang = [90,90];
+    ang = [90,90,90,90];
     
     
     d1 = dist(self.l6,0,x,y)
     print("d1 = %f" % d1)
-    beta = acos((self.l2**2+d1**2-self.l5**2)/(2*self.l5*d1))
+    beta = acos((self.l2**2+d1**2-self.l5**2)/(2*self.l2*d1))
     print("beta = %f" % degrees(beta))
     alpha = atan2(y,x-self.l6)
     print("alpha = %f" % degrees(alpha))
@@ -46,7 +46,7 @@ class Arm( object ):
     print("yc = %f" % yc)
     
     theta4 = -atan2(yc-y,x-xc)
-    
+    ang[3]=theta4;
     
     #theta4 = acos((x-self.l2*cos(theta2))/(self.l5))
     print("theta4 = %f" % degrees(theta4))
@@ -66,10 +66,43 @@ class Arm( object ):
     print("theta1= %f" % degrees(theta1))
     ang[0] = theta1
     
+    theta3 = acos((self.l1**2+self.l3**2-d2**2)/(2*self.l1*self.l3))
+    ang[2] = theta3
     return ang
+    
+  def plot3D(self,ang):
+    xc = self.l2*cos(ang[1]) + self.l6
+    yc = self.l2*sin(ang[1])
+    xe = self.l5*cos(ang[3])+xc
+    ye = self.l5*sin(ang[3])+yc
+    xa = -self.l7+self.l1*cos(ang[0])
+    ya = self.l1*sin(ang[0])
+    xb = self.l3*cos(ang[2])+xa
+    yb = self.l3*sin(ang[3])+ya
+    
+    x = [-self.l7, xa]
+    y = [0,ya]
+    z = [0,0]
+    ax.plot(x,y,z)
+    x = [xa, xb]
+    y = [ya, yb]
+    ax.plot(x,y,z)
+    x = [xb,xc]
+    y = [yb,yc]
+    ax.plot(x,y,z)
+    x = [self.l6, xc]
+    y = [0,yc]
+    ax.plot(x,y,z)
+    x = [xc,xe]
+    y= [yc,ye]
+    
     
     
 def main():
+  global fig, ax,x,y,z
+  fig = gcf()
+  ax = fig.gca(projection='3d')
+  
   
   a = Arm()
   
@@ -77,7 +110,7 @@ def main():
   y = 5
   
   ang = a.angFromEnd(x,y)
-  
+  a.plot3D(ang)
   print(ang)
   
   
